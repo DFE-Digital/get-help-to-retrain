2fbcd5c85d935c1982a8c426439766774ee52d2c
Andrew Retmanski <andrew.retmanski@digital.education.gov.uk>
2019-06-21 13:43:42 +0100

    Add rake task to import sitemap
    This combines scraping the sitemap from Explore My careers and builds placeholder category and job profile models for all relevant links found in the sitemap. We need to build these models before they can be linked together whilst scraping the category pages later.

---
+++ lib/tasks/data_import/import_sitemap.rake
@@ -0,0 +1,20 @@
+namespace :data_import do
+
+  # bin/rails data_import:import_sitemap
+  task import_sitemap: :environment do
+
+    print 'Importing sitemap from National Careers Service site'
+    sitemap = SitemapScraper.new
+    sitemap.scrape
+
+    sitemap.categories.each do |slug, url|
+      print "Found category page #{url}"
+      Category.import(slug, url)
+    end
+
+    sitemap.job_profiles.each do |slug, url|
+      print "Found job profile page #{url}"
+      JobProfile.import(slug, url)
+    end
+  end
+end
---
+++ spec/tasks/import_sitemap_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+require 'support/tasks'
+
+describe 'data_import:import_sitemap', type: :task do
+  let!(:fake_scraper) { SitemapScraper.new }
+
+  before do
+    expect(SitemapScraper).to receive(:new) { fake_scraper }
+    expect(fake_scraper).to receive(:scrape)
+  end
+
+  it 'creates categories' do
+    expect(fake_scraper).to receive(:categories) do
+      {
+        'administration' => 'https://nationalcareers.service.gov.uk/job-categories/administration',
+        'business-and-finance' => 'https://nationalcareers.service.gov.uk/job-categories/business-and-finance'
+      }
+    end
+    expect(fake_scraper).to receive(:job_profiles) do
+      {}
+    end
+
+    expect { task.execute }.to change { Category.count }.by(2)
+  end
+
+  it 'creates jobs' do
+    expect(fake_scraper).to receive(:categories) do
+      {}
+    end
+    expect(fake_scraper).to receive(:job_profiles) do
+      {
+        'admin-assistant' => 'https://nationalcareers.service.gov.uk/job-profiles/admin-assistant',
+        'finance-officer' => 'https://nationalcareers.service.gov.uk/job-profiles/finance-officer'
+      }
+    end
+
+    expect { task.execute }.to change { JobProfile.count }.by(2)
+  end
+end
