beaedcfc95fdff1a8f8787f687a90f346a7df333
Octavian Neguletu <octavian.neguletu@digital.education.gov.uk>
2019-06-27 11:13:59 +0100

    Refactor job_profiles controller

--- app/controllers/job_profiles_controller.rb
+++ app/controllers/job_profiles_controller.rb
@@ -1,13 +1,11 @@
 class JobProfilesController < ApplicationController
-  before_action :decorate_job_profile, on: %i[show skills]
+  def show
+    @job_profile = JobProfileDecorator.new(resource)
+  end
 
   private
 
-  def job_profile
-    @job_profile ||= JobProfile.find_by(slug: params[:id])
-  end
-
-  def decorate_job_profile
-    @job_profile = JobProfileDecorator.new(job_profile)
+  def resource
+    @resource ||= JobProfile.find_by(slug: params[:id])
   end
 end
