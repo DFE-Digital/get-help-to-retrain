a2ab00986892fde2bea70c0e7d03f03b9faeb0b1
Octavian Neguletu <octavian.neguletu@digital.education.gov.uk>
2019-06-27 16:49:24 +0100

    Fix single quote inconsistency

--- features/step_definitions/test_steps.rb
+++ features/step_definitions/test_steps.rb
@@ -13,7 +13,7 @@ end
 Then('the correct eligibility criteria is displayed') do
   expect(page).to have_content('you\'re employed')
   expect(page).to have_content('you do not have a degree')
-  expect(page).to have_content('you’re based in the Liverpool area')
+  expect(page).to have_content('you\'re based in the Liverpool area')
   expect(page).to have_content('aged 24 or over')
   expect(page).to have_content('you\'re earning below £35,000 per year')
 end
