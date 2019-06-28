2a9ce132e71f37210368f9549af2ca5bb175377b
cwrw <abeer.salameh@gmail.com>
2019-06-28 09:50:36 +0100

    GET-138 Rely on params to decide breadcrumbs
    jira-ticket: https://dfedigital.atlassian.net/browse/GET-138
    
    * Move results list to specific directories to avoid confusion
      as now we are being explicit with passing params around
    * Change breadcrumsb helper to rely on params rather than referer
      request as that was flakey and not explicit causing unexpected
      behaviour from outside requests and if someone presses the back
      button on the browser

--- spec/helpers/job_profiles_helper_spec.rb
+++ spec/helpers/job_profiles_helper_spec.rb
@@ -2,12 +2,24 @@ require 'rails_helper'
 
 RSpec.describe JobProfilesHelper do
   describe '.job_profile_breadcrumbs_for' do
-    it 'returns title for categories if referer path is a category' do
-      expect(helper.job_profile_breadcrumbs_for('categories')).to eq(t('breadcrumb.job_category'))
+    it 'returns categories breadcrumbs if referer path is a category' do
+      params = { category: 1 }
+      expect(helper.job_profile_breadcrumbs_for(params)).to eq(
+        [t('breadcrumb.job_category'), category_path(1)]
+      )
     end
 
-    it 'defaults to results page title' do
-      expect(helper.job_profile_breadcrumbs_for('results')).to eq(t('breadcrumb.search_results'))
+    it 'returns explore occupations results breadcrumbs if referer path is a defined result' do
+      params = { explore_occupations_result: 'Developer' }
+      expect(helper.job_profile_breadcrumbs_for(params)).to eq(
+        [t('breadcrumb.search_results'), results_explore_occupations_path(name: 'Developer')]
+      )
+    end
+
+    it 'defaults to empty explore occupations results' do
+      expect(helper.job_profile_breadcrumbs_for({})).to eq(
+        [t('breadcrumb.search_results'), results_explore_occupations_path]
+      )
     end
   end
 end
