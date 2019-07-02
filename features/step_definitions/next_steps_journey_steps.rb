baf169f1c354ab69b1258df3447546f22cb0516c
Paul Littlebury <paullittlebury@gmail.com>
2019-07-02 17:07:23 +0200

    Corrected next journey steps file for rubocop

--- features/step_definitions/next_steps_journey_steps.rb
+++ features/step_definitions/next_steps_journey_steps.rb
@@ -1,6 +1,6 @@
 Then('I can learn what all my next options are') do
-  lineValues =~ ['Advice on how to look for and apply for jobs', 'Find a job', 'Find an apprenticeship']
-  lineValues.each do |item|
+  linevalues = ['Advice on how to look for and apply for jobs', 'Find a job', 'Find an apprenticeship']
+  linevalues.each do |item|
     expect(page).to have_content(item)
   end
 end
