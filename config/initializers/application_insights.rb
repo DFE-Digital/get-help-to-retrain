aab9d0793a58d004a98bb9cc3e4aa6a59e43d37e
Andrew Retmanski <andrew.retmanski@digital.education.gov.uk>
2019-07-02 21:19:38 +0100

    Omit unneeded telemetry setup until this is needed later Left as commented sample code to provide a reference later, but we don't need to create these objects now.

--- config/initializers/application_insights.rb
+++ config/initializers/application_insights.rb
@@ -16,21 +16,21 @@ if Rails.env.production?
     # # https://github.com/microsoft/ApplicationInsights-Ruby
     # # https://github.com/microsoft/ApplicationInsights-Home/wiki#getting-an-application-insights-instrumentation-key
     #
-    sender = ApplicationInsights::Channel::AsynchronousSender.new
-    queue = ApplicationInsights::Channel::AsynchronousQueue.new(sender)
-    channel = ApplicationInsights::Channel::TelemetryChannel.new(nil, queue)
-
-    TELEMETRY_CLIENT = ApplicationInsights::TelemetryClient.new(app_insights_key, channel).tap do |tc|
-      # flush telemetry if we have 10 or more telemetry items in our queue
-      tc.channel.queue.max_queue_length = 10
-      # send telemetry to the service in batches of 5
-      tc.channel.sender.send_buffer_size = 5
-      # the background worker thread will be active for 5 seconds before it shuts down. if
-      # during this time items are picked up from the queue, the timer is reset.
-      tc.channel.sender.send_time = 5
-      # the background worker thread will poll the queue every 0.5 seconds for new items
-      tc.channel.sender.send_interval = 0.5
-    end
+    # sender = ApplicationInsights::Channel::AsynchronousSender.new
+    # queue = ApplicationInsights::Channel::AsynchronousQueue.new(sender)
+    # channel = ApplicationInsights::Channel::TelemetryChannel.new(nil, queue)
+    #
+    # TELEMETRY_CLIENT = ApplicationInsights::TelemetryClient.new(app_insights_key, channel).tap do |tc|
+    #   # flush telemetry if we have 10 or more telemetry items in our queue
+    #   tc.channel.queue.max_queue_length = 10
+    #   # send telemetry to the service in batches of 5
+    #   tc.channel.sender.send_buffer_size = 5
+    #   # the background worker thread will be active for 5 seconds before it shuts down. if
+    #   # during this time items are picked up from the queue, the timer is reset.
+    #   tc.channel.sender.send_time = 5
+    #   # the background worker thread will poll the queue every 0.5 seconds for new items
+    #   tc.channel.sender.send_interval = 0.5
+    # end
 
     # setup unhandled exception handler
     ApplicationInsights::UnhandledException.collect(app_insights_key)
