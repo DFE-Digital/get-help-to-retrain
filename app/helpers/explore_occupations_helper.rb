331df8f3f84965e815ead4d1dfcf2d0df09e6c88
Miguel Afonso <miguel.afonso@digital.education.gov.uk>
2019-07-03 11:32:23 +0100

    GET-158 Support optional custom hostname

--- app/helpers/explore_occupations_helper.rb
+++ app/helpers/explore_occupations_helper.rb
@@ -1,6 +1,6 @@
 module ExploreOccupationsHelper
   def job_profile_category_list(job_profile)
-    job_profile.categories.map { |category|
+    job_profile.categories.map {|category|
       link_to(category.name, category_path(category.slug), class: 'govuk-link')
     }.join(', ').html_safe
   end
