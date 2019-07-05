5fa1b5e68d8c33792a4ad826c9fabbb0514a1e1f
Paul Littlebury <paullittlebury@gmail.com>
2019-07-05 09:10:14 +0200

    Corrected rubocop reported format error

--- features/step_definitions/job_profile_steps.rb
+++ features/step_definitions/job_profile_steps.rb
@@ -3,12 +3,19 @@ Given('a job profile exists with the name {string}') do |name|
 end
 
 Given('a job profile exists for a Construction manager') do
-  create(:job_profile, :with_html_content, name: 'Construction manager', salary_min: 27_000, salary_max: 70_000, skills: [
-    create(:skill, name: 'pass enhanced background checks'),
-    create(:skill, name: 'to be able to carry out basic tasks on a computer or hand-held device'),
-    create(:skill, name: 'thinking and reasoning skills'),
-    create(:skill, name: 'excellent verbal communication skills')
-  ])
+  create(
+    :job_profile,
+    :with_html_content,
+    name: 'Construction manager',
+    salary_min: 27_000,
+    salary_max: 70_000,
+    skills: [
+      create(:skill, name: 'pass enhanced background checks'),
+      create(:skill, name: 'to be able to carry out basic tasks on a computer or hand-held device'),
+      create(:skill, name: 'thinking and reasoning skills'),
+      create(:skill, name: 'excellent verbal communication skills')
+    ]
+  )
 end
 
 When('I am on the {string} job profile page') do |name|
