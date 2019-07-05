91f37480e8ae20855a2bc6f87f036098259250d1
Andrew Retmanski <andrew.retmanski@digital.education.gov.uk>
2019-07-05 12:50:22 +0100

    Favour use of Loggly logging if both env vars are set

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,14 +77,14 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Use a different logger for distributed setups.
-  if ENV['RAILS_LOG_TO_STDOUT'].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
-    logger.formatter = config.log_formatter
-    config.logger    = ActiveSupport::TaggedLogging.new(logger)
-  elsif ENV['LOGGLY_TOKEN'].present?
+  if ENV['LOGGLY_TOKEN'].present?
     loggly_url = "https://logs-01.loggly.com/inputs/#{ENV['LOGGLY_TOKEN']}/tag/ruby/"
     config.logger = Logglier.new(loggly_url, threaded: true)
     config.logger.formatter = config.log_formatter
+  elsif ENV['RAILS_LOG_TO_STDOUT'].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
   # Do not dump schema after migrations.
