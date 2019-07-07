bb4aeaafe09455c7769e0e893f5420ef56150032
Paul Littlebury <paullittlebury@gmail.com>
2019-07-07 20:26:45 +0200

    added step file for search results

--- features/step_definitions/search_results.rb
+++ features/step_definitions/search_results.rb
@@ -1,3 +1,3 @@
 Then('I should see the alternative titles under the first result title') do
-  find("li.govuk-\!-padding-bottom-3:nth-child(1) > p:nth-child(2)").text.should != ''
+  expect(page).to have_field('p.govuk-body-s:nth-child(2)', with: /.+/) 
 end
