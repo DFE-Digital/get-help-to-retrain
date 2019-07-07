4fb201d91cfbf4b618d0a7c1fcdb06c0c2e09595
Paul Littlebury <paullittlebury@gmail.com>
2019-07-07 21:06:33 +0200

    Correctedlint error in search_results.rb

--- features/step_definitions/search_results.rb
+++ features/step_definitions/search_results.rb
@@ -1,3 +1,3 @@
 Then('I should see the alternative titles under the first result title') do
-  expect(page).to have_field('p.govuk-body-s:nth-child(2)', with: /.+/) 
+  expect(page).to have_field('p.govuk-body-s:nth-child(2)', with: /.+/)
 end
