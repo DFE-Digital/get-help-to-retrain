dac55398461801289bfba0c03751a895763f5fc2
Octavian Neguletu <octavian.neguletu@digital.education.gov.uk>
2019-07-18 16:53:59 +0100

    Fix rubocop violation + small refactor

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -2,13 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Course, type: :model do
   describe 'validations' do
-    let(:course) { build(:course, email: 'weirdemail@') }
-
     describe '#email' do
-      context 'when the email is invalid' do
-        it 'resource is not valid' do
-          expect(course).not_to be_valid
-        end
+      let(:course) { build(:course, email: 'weirdemail@') }
+
+      it 'is not valid' do
+        expect(course).not_to be_valid
       end
     end
   end
