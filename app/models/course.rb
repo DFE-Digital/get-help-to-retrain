5b2639ddd5a1475077ccc1694c0a73810a0289d1
cwrw <abeer.salameh@gmail.com>
2019-07-19 13:47:43 +0100

    GET-123 Geocode postcodes on courses model
    jira-ticket: https://dfedigital.atlassian.net/browse/GET-123
    
    * Add Geocode test helpers and default coordinates to prevent
      any calls in rspec
    * validate postcode on course model against uk postcodes
    * format postcode on course model against uk postcodes to
      be in capital letter format, and a space in the middle
    * add callback to geocode the postcodes after validation
      on courses model. This means latitude and longitude
      values will be populated by the Geocoder gem
    * Only run this geocoding if the postcode has changed,
      and the latitude and longitude values have not been
      manually entered

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,15 @@
 class Course < ApplicationRecord
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :postcode, presence: true, postcode: true
+  validates_presence_of :title, :url, :provider, :topic
 
-  validates_presence_of :title, :url, :provider, :topic, :postcode
+  geocoded_by :postcode
+  after_validation :format_postcode
+  after_validation :geocode, if: -> { postcode_changed? && !latitude_changed? && !latitude_changed? }
 
+  def format_postcode
+    return unless postcode
+
+    self.postcode = UKPostcode.parse(postcode).to_s
+  end
 end
