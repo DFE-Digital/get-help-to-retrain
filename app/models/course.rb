32e83be5d358bfc1d57791ed10cb9428dda8b172
cwrw <abeer.salameh@gmail.com>
2019-07-20 11:30:41 +0100

    GET-123 Refactor Find courses and extract into it's own class
    jira-ticket: https://dfedigital.atlassian.net/browse/GET-123
    
    Extract geospatial search logic out of the course model and into
    it's own class, making for easier logic to follow as well as
    testing.

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,4 @@
 class Course < ApplicationRecord
-  InvalidPostcodeError = Class.new(StandardError)
-  GeocoderAPIError = Class.new(StandardError)
-
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :postcode, presence: true, postcode: true
   validates_presence_of :title, :url, :provider, :topic
@@ -10,24 +7,8 @@ class Course < ApplicationRecord
   after_validation :format_postcode
   after_validation :geocode, if: -> { postcode_changed? && !latitude_changed? && !longitude_changed? }
 
-  def self.find_courses_near(search_postcode:, topic: nil, distance: 5)
-    return none unless search_postcode.present?
-
-    coordinates = Geocoder.coordinates(parse_postcode(search_postcode))
-
-    courses = near(coordinates, distance, units: :mi, order: :distance)
-    courses = courses.where(topic: topic) if topic.present?
-    courses
-  rescue SocketError, Timeout::Error, Geocoder::Error => e
-    Rails.logger.error("Geocoder API error: #{e.message}")
-    raise GeocoderAPIError
-  end
-
-  def self.parse_postcode(str)
-    uk_postcode = UKPostcode.parse(str)
-    raise InvalidPostcodeError unless uk_postcode.full_valid?
-
-    uk_postcode.to_s
+  def self.find_courses_near(postcode:, topic: nil, distance: 5)
+    CourseGeospatialSearch.new(postcode: postcode, topic: topic, distance: distance).find_courses
   end
 
   def format_postcode
--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -2,14 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Course, type: :model do
   describe '.find_courses_near' do
-    it 'returns nothing if no postcode entered' do
-      expect(described_class.find_courses_near(search_postcode: nil)).to be_empty
-    end
-
-    it 'returns nothing if empty postcode entered' do
-      expect(described_class.find_courses_near(search_postcode: '')).to be_empty
-    end
-
     it 'returns courses ordered by distance to postcode entered' do
       Geocoder::Lookup::Test.add_stub(
         'NW6 8ET', [{ 'coordinates' => [0.1, 1] }]
@@ -19,12 +11,12 @@ RSpec.describe Course, type: :model do
       course2 = create(:course, latitude: 0.1, longitude: 1.003)
       course3 = create(:course, latitude: 0.1, longitude: 1.002)
 
-      expect(described_class.find_courses_near(search_postcode: 'NW6 8ET', distance: 2)).to eq(
+      expect(described_class.find_courses_near(postcode: 'NW6 8ET', distance: 2)).to eq(
         [course1, course3, course2]
       )
     end
 
-    it 'scopes courses by topic' do
+    it 'returns courses ordered by distance to postcode entered and scoped by topic' do
       Geocoder::Lookup::Test.add_stub(
         'NW6 8ET', [{ 'coordinates' => [0.1, 1] }]
       )
@@ -33,45 +25,9 @@ RSpec.describe Course, type: :model do
       course2 = create(:course, latitude: 0.1, longitude: 1, topic: 'english')
       create(:course, latitude: 0.1, longitude: 1, topic: 'maths')
 
-      expect(described_class.find_courses_near(search_postcode: 'NW6 8ET', distance: 2, topic: 'english')).to contain_exactly(
+      expect(described_class.find_courses_near(postcode: 'NW6 8ET', distance: 2, topic: 'english')).to contain_exactly(
         course2
       )
-    end
-
-    it 'limist courses to distance entered from postcode' do
-      Geocoder::Lookup::Test.add_stub(
-        'NW6 8ET', [{ 'coordinates' => [0.1, 1] }]
-      )
-
-      course1 = create(:course, latitude: 0.1, longitude: 1.001)
-      create(:course, latitude: 0.1, longitude: 1.2)
-      create(:course, latitude: 0.1, longitude: 1.3)
-
-      expect(described_class.find_courses_near(search_postcode: 'NW6 8ET', distance: 1)).to eq(
-        [course1]
-      )
-    end
-
-    it 'returns nothing if postcode entered is too far' do
-      Geocoder::Lookup::Test.add_stub(
-        'NW6 8ET', [{ 'coordinates' => [0.1, 1] }]
-      )
-
-      create(:course, latitude: 0.1, longitude: 2)
-      create(:course, latitude: 0.1, longitude: 3)
-      create(:course, latitude: 0.1, longitude: 4)
-
-      expect(described_class.find_courses_near(search_postcode: 'NW6 8ET', distance: 10)).to be_empty
-    end
-
-    it 'raises error if postcode entered if invalid' do
-      expect { described_class.find_courses_near(search_postcode: 'NW6 ET') }.to raise_error(described_class::InvalidPostcodeError)
-    end
-
-    it 'raises error if API for geocoding not available' do
-      allow(Geocoder).to receive(:coordinates).and_raise(Geocoder::ServiceUnavailable)
-
-      expect { described_class.find_courses_near(search_postcode: 'NW6 8ET') }.to raise_error(described_class::GeocoderAPIError)
     end
   end
 
