3d92075fda9674d807c098c24a64824863b60f45
Octavian Neguletu <octavian.neguletu@digital.education.gov.uk>
2019-07-25 11:01:22 +0100

    Add routing + feature specs

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   get 'task-list', to: 'pages#task_list'
   get 'find-training-courses', to: 'pages#find_training_courses'
   get 'next-steps', to: 'pages#next_steps'
+  get 'maths-overview', to: 'pages#maths_overview'
+  get 'english-overview', to: 'pages#english_overview'
 
   resources :courses, path: 'courses/:topic_id', only: %i[index], constraints: { topic_id: /maths|english/ }
 
---
+++ spec/features/course_overview_pages_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.feature 'Course overview page', type: :feature do
+  scenario 'User navigates to maths overview page' do
+    visit(maths_overview_path)
+
+    expect(page).to have_text('Benefits of doing a maths course')
+  end
+
+  scenario 'User continues journey to explore maths courses' do
+    create(:course, :maths)
+
+    visit(maths_overview_path)
+
+    click_on('Find a maths course')
+
+    expect(page).to have_text('Maths courses near me')
+  end
+
+  scenario 'User navigates to english overview page' do
+    visit(english_overview_path)
+
+    expect(page).to have_text('Benefits of an English course')
+  end
+
+  scenario 'User continues journey to explore English courses' do
+    create(:course, :english)
+
+    visit(english_overview_path)
+
+    click_on('Find an English course')
+
+    expect(page).to have_text('English courses near me')
+  end
+end
