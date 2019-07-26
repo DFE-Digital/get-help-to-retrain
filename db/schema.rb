dddc1b781de07dec13c215fa07c030b58789dddb
Andrew Retmanski <aretmanski@gmail.com>
2019-07-26 12:47:34 +0100

    Add `enabled` attribute for skills This will be used during the forthcoming spike to explore use of skills for matching job profiles. We know that not all of the skills will be used for this, so this adds the ability to disable some of those. This capability won't be used programatically yet, but adding the column allows the data to be setup in advance and exposed via the admin interface.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_19_081758) do
+ActiveRecord::Schema.define(version: 2019_07_24_160424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,6 +94,8 @@ ActiveRecord::Schema.define(version: 2019_07_19_081758) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "enabled", default: true, null: false
+    t.index ["enabled"], name: "index_skills_on_enabled"
     t.index ["name"], name: "index_skills_on_name"
   end
 
