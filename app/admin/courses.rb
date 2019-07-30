36e8a7cf3f9530ecc69f8f46bff124dac22e2ca4
Andrew Retmanski <aretmanski@gmail.com>
2019-07-30 14:31:43 +0100

    Fix editing of courses in admin pages This would throw an exception when saving course changes otherwise. Added a whitelist of editable attributes.

--- app/admin/courses.rb
+++ app/admin/courses.rb
@@ -1,1 +1,6 @@
-ActiveAdmin.register Course if defined?(ActiveAdmin)
+if defined?(ActiveAdmin)
+  ActiveAdmin.register Course do
+    permit_params :title, :provider, :url, :address_line1, :address_line2, :town, :county, :postcode,
+                  :email, :topic, :phone_number, :active, :latitude, :longitude
+  end
+end
