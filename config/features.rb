42b118325d3ee38a478639e962f158cb1e8e9d4f
cwrw <abeer.salameh@gmail.com>
2019-07-30 14:42:20 +0100

    GET-36 Use query string only in dev as they won't work at route level anyway
    jira-ticket: https://dfedigital.atlassian.net/browse/GET-36

--- config/features.rb
+++ config/features.rb
@@ -2,7 +2,7 @@ require 'flipflop/split_client_strategy'
 
 Flipflop.configure do
   strategy :test unless Rails.env.production?
-  strategy :query_string
+  strategy :query_string unless Rails.env.production?
   strategy :split_client, api_key: ENV['SPLIT_API_KEY'] if ENV['SPLIT_API_KEY'].present?
 
   feature :foo, description: 'Example feature flag', default: true
