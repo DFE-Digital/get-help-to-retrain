9d6883846156fc3c7b27b73855301023307398cd
cwrw <abeer.salameh@gmail.com>
2019-07-31 11:14:01 +0100

    GET-133 Fix feedback survey stylings
    jira-ticket: https://dfedigital.atlassian.net/browse/GET-39
    
    * Separate ids/classes for js feedback survey
    * css amendments to survey and cleanup
    * Move survey container to main div

--- app/webpacker/packs/user-feedback.js
+++ app/webpacker/packs/user-feedback.js
@@ -1,8 +1,8 @@
 
 function UserFeedback () {
   this.start = function () {
-    var yesLink = document.querySelector('.answer-yes');
-    var noLink = document.querySelector('.answer-no');
+    var yesLink = document.querySelector('#answer-yes');
+    var noLink = document.querySelector('#answer-no');
 
     handleSuccessAnswer(yesLink);
     handleRejectionAnswer(noLink);
@@ -11,16 +11,16 @@ function UserFeedback () {
   function handleSuccessAnswer(element) {
     element.onclick = function(e) {
       e.preventDefault();
-      document.querySelector('.feedback-prompt-full-question').classList.add('hidden');
-      document.querySelector('.feedback-prompt-success').classList.remove('hidden');
+      document.querySelector('#feedback-prompt-question').classList.add('hidden');
+      document.querySelector('#feedback-prompt-success').classList.remove('hidden');
     }
   }
 
   function handleRejectionAnswer(element) {
     element.onclick = function(e) {
       e.preventDefault();
-      document.querySelector('.feedback-prompt-questions').classList.remove('hidden');
-      document.querySelector('.feedback-prompt').remove();
+      document.querySelector('#feedback-survey-container').classList.remove('hidden');
+      document.querySelector('#feedback-prompt-container').remove();
     }
   }
 }
