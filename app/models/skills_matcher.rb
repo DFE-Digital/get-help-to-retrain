eae8a66664c42454a8405bf412d6018aa2d93a8a
cwrw <abeer.salameh@gmail.com>
2019-08-14 12:26:00 +0100

    GET-232 Escape all injected sql and tidy up query
    jira-ticket: https://dfedigital.atlassian.net/browse/GET-232

--- app/models/skills_matcher.rb
+++ app/models/skills_matcher.rb
@@ -13,8 +13,9 @@ class SkillsMatcher
   end
 
   def job_profile_scores
+    user_skills_ids_count = user_skill_ids.count
     @job_profile_scores ||= build_query.each_with_object({}) do |job_profile, hash|
-      hash[job_profile.id] = job_profile.skills_matched.to_f / user_skill_ids.count * 100
+      hash[job_profile.id] = job_profile.skills_matched.to_f / user_skills_ids_count * 100
     end
   end
 
@@ -22,7 +23,7 @@ class SkillsMatcher
 
   def job_profile_skills_subquery
     JobProfileSkill
-      .select('job_profile_id, COUNT(job_profile_id) AS skills_matched')
+      .select(:job_profile_id, 'COUNT(job_profile_id) AS skills_matched')
       .where(skill_id: user_skill_ids)
       .group(:job_profile_id)
       .to_sql
@@ -30,12 +31,12 @@ class SkillsMatcher
 
   def job_profiles_subquery
     JobProfile
-      .select('skills_matched, array_agg(id order by name ASC) as alphabetically_ordered_ids')
-      .from("(#{job_profile_skills_subquery}) as ranked_job_profiles")
-      .joins('LEFT OUTER JOIN "job_profiles" ON "job_profiles"."id" = "ranked_job_profiles"."job_profile_id"')
+      .select(:skills_matched, 'array_agg(id order by name ASC) as alphabetically_ordered_ids')
+      .from(Arel.sql("(#{job_profile_skills_subquery}) as ranked_job_profiles"))
+      .joins('LEFT JOIN job_profiles ON job_profiles.id = ranked_job_profiles.job_profile_id')
       .where.not(id: user_job_profile_ids)
       .group(:skills_matched)
-      .order('skills_matched desc')
+      .order(skills_matched: :desc)
       .to_sql
   end
 
@@ -45,11 +46,13 @@ class SkillsMatcher
         .select(
           :skills_matched, :id, :name, :description, :alternative_titles, :salary_min, :salary_max, :slug, :growth
         )
-        .from(
-          "(#{job_profiles_subquery}) as ordered_query, unnest(alphabetically_ordered_ids) WITH ORDINALITY ordered_id"
-        )
-        .joins('LEFT JOIN "job_profiles" ON "job_profiles"."id" = "ordered_id"')
+        .from(Arel.sql(job_profile_ids_unnest_query))
+        .joins('LEFT JOIN job_profiles ON job_profiles.id = ordered_id')
     end
+  end
+
+  def job_profile_ids_unnest_query
+    "(#{job_profiles_subquery}) as ordered_query, unnest(alphabetically_ordered_ids) WITH ORDINALITY ordered_id"
   end
 
   def user_skill_ids
