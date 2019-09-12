1d83ab47a7bc363ec5d5644d31d62e6e9ed41fb8
Octavian Neguletu <octavian.neguletu@digital.education.gov.uk>
2019-09-12 17:14:22 +0100

    Code sample

---
+++ app/models/dwp_client.rb
@@ -0,0 +1,28 @@
+class DwpClient
+  def search(payload)
+    response = connection.get(api_base.concat(payload.merge(api_key: api_key, api_id: api_id).to_query)).body
+
+    JSON.parse(response)
+  end
+
+  private
+
+  def api_base
+    @api_base ||= 'https://findajob.dwp.gov.uk/api/search?'
+  end
+
+  def api_key
+    ENV['DWP_API_KEY']
+  end
+
+  def api_id
+    ENV['DWP_API_ID']
+  end
+
+  def connection
+    @connection ||= Faraday.new(url: api_base) do |faraday|
+      faraday.response :logger, ::Logger.new(STDOUT), bodies: true
+      faraday.adapter Faraday.default_adapter
+    end
+  end
+end
