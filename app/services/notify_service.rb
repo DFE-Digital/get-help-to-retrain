aa3cb0de07487040172c5d7b3212ae8c139f2fd1
Andrew Retmanski <andrew.retmanski@digital.education.gov.uk>
2019-09-13 14:29:21 +0100

    Move NOTIFY_API_KEY access to Rails configuration This standardises approach for reading ENV vars.

--- app/services/notify_service.rb
+++ app/services/notify_service.rb
@@ -5,7 +5,7 @@ class NotifyService
   CONFIRMATION_TEMPLATE_ID = 'b6b33372-f57b-4603-b8d2-8f66b4cebf24'.freeze
   LINK_TO_RESULTS_TEMPLATE_ID = '7d186e79-31d6-4f11-b5d9-5f1f83b0d159'.freeze
 
-  def initialize(api_key: ENV['NOTIFY_API_KEY'])
+  def initialize(api_key: Rails.configuration.notify_api_key)
     @api_key = api_key
   end
 
