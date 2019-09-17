20bc20e8dc8c1b5e9076bf8b2c9306d389335a19
cwrw <abeer.salameh@gmail.com>
2019-09-17 10:51:53 +0100

    Add new confirmation id

--- app/services/notify_service.rb
+++ app/services/notify_service.rb
@@ -2,7 +2,7 @@ require 'notifications/client'
 
 class NotifyService
   NotifyAPIError = Class.new(StandardError)
-  CONFIRMATION_TEMPLATE_ID = 'b6b33372-f57b-4603-b8d2-8f66b4cebf24'.freeze
+  CONFIRMATION_TEMPLATE_ID = '02534375-5b5e-4da7-9884-009715421301'.freeze
   LINK_TO_RESULTS_TEMPLATE_ID = '7d186e79-31d6-4f11-b5d9-5f1f83b0d159'.freeze
 
   def initialize(api_key: Rails.configuration.notify_api_key)
