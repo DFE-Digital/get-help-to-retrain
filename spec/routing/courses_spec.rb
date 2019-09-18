1f05083a4df507d4e95a9b17a5179802b987c331
cwrw <abeer.salameh@gmail.com>
2019-09-18 09:28:11 +0100

    GET-387 Move controller specs to route/request specs appropriatly
    jira-ticket: https://dfedigital.atlassian.net/browse/GET-387

--- spec/routing/courses_spec.rb
+++ spec/routing/courses_spec.rb
@@ -12,4 +12,14 @@ RSpec.describe 'routes for Courses', type: :routing do
   it 'successfully routes to pages#training_hub' do
     expect(get(training_hub_path)).to route_to('pages#training_hub')
   end
+
+  it 'successfully routes to courses#index for existing course' do
+    create(:course, :maths)
+
+    expect(get('/courses/maths')).to route_to(controller: 'courses', action: 'index', topic_id: 'maths')
+  end
+
+  it 'does not route to courses#index for non-existing course' do
+    expect(get('/courses/history')).not_to be_routable
+  end
 end
