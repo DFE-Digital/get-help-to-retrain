fce63adbd4673c3cdb5131c5d6a853378c740649
Andrew Retmanski <andrew.retmanski@digital.education.gov.uk>
2019-09-25 13:56:30 +0100

    Include git commit sha for sentry release context

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,5 +2,6 @@ if Rails.configuration.sentry_dsn.present?
   Raven.configure do |config|
     config.dsn = Rails.configuration.sentry_dsn
     config.sanitize_fields = Rails.configuration.filter_parameters.map(&:to_s)
+    config.release = Rails.configuration.git_commit
   end
 end
