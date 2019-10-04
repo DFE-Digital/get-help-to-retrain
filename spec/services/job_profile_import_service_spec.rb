8205e76672f89a93db8f943158d8dc251ba7867d
Andrew Retmanski <andrew.retmanski@digital.education.gov.uk>
2019-10-04 16:44:36 +0100

    Extend job profile import service to support hidden titles spreadsheet

--- spec/services/job_profile_import_service_spec.rb
+++ spec/services/job_profile_import_service_spec.rb
@@ -3,12 +3,13 @@ require 'rails_helper'
 RSpec.describe JobProfileImportService do
   subject(:importer) { described_class.new }
 
-  let(:path) { Rails.root.join('spec', 'fixtures', 'files', 'job_profile_growth.xlsx').to_s }
+  let(:growth_path) { Rails.root.join('spec', 'fixtures', 'files', 'job_profile_growth.xlsx').to_s }
+  let(:titles_path) { Rails.root.join('spec', 'fixtures', 'files', 'job_profile_hidden_titles.xlsx').to_s }
   let(:ceo) { JobProfile.find_by_name('Chief executive') }
 
   before do
-    create :job_profile, name: 'Chief executive'
-    create :job_profile, name: 'Monkey handler'
+    create :job_profile, name: 'Chief executive', slug: 'chief-executive'
+    create :job_profile, name: 'Monkey handler', slug: 'monkey-handler'
   end
 
   describe '#import_growth' do
@@ -17,23 +18,51 @@ RSpec.describe JobProfileImportService do
     end
 
     it 'does not raise error with valid filename' do
-      expect { importer.import_growth(path) }.not_to raise_exception
+      expect { importer.import_growth(growth_path) }.not_to raise_exception
     end
 
     it 'updates matching job profiles' do
-      importer.import_growth(path)
+      importer.import_growth(growth_path)
       expect(ceo).to have_attributes(soc: '1115', extended_soc: '1115A', growth: (a_value > 72))
     end
   end
 
   describe '#import_growth_stats' do
-    before { importer.import_growth(path) }
+    before { importer.import_growth(growth_path) }
 
     it 'reports statistics on completion' do
       expect(importer.import_growth_stats).to eq(
         job_profiles_total: 2,
         job_profiles_with_growth: 1,
         job_profiles_missing_growth: 1,
+        errors: 2
+      )
+    end
+  end
+
+  describe '#import_hidden_titles' do
+    it 'raises error if invalid filename' do
+      expect { importer.import_hidden_titles('foo.xlsx') }.to raise_exception(IOError)
+    end
+
+    it 'does not raise error with valid filename' do
+      expect { importer.import_hidden_titles(titles_path) }.not_to raise_exception
+    end
+
+    it 'updates matching job profiles' do
+      importer.import_hidden_titles(titles_path)
+      expect(ceo).to have_attributes(hidden_titles: 'Boss man,Big cheese')
+    end
+  end
+
+  describe '#import_hidden_titles_stats' do
+    before { importer.import_hidden_titles(titles_path) }
+
+    it 'reports statistics on completion' do
+      expect(importer.import_hidden_titles_stats).to eq(
+        job_profiles_total: 2,
+        job_profiles_with_hidden_titles: 1,
+        job_profiles_missing_hidden_titles: 1,
         errors: 2
       )
     end
