6f3ee970403b7559266d6381a04c9cf319561261
Andrew Retmanski <andrew.retmanski@digital.education.gov.uk>
2019-10-11 16:23:29 +0100

    Minor fixes and review feedback

--- app/webpacker/packs/cookies-banner.js
+++ app/webpacker/packs/cookies-banner.js
@@ -17,6 +17,7 @@ function CookiesBanner () {
 
   function setCookie(name, value, days) {
     var date = new Date;
+
     date.setTime(date.getTime() + 24*60*60*1000*days);
     document.cookie = name + "=" + value + ";path=/;expires=" + date.toGMTString();
   }
