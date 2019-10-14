1c53bf5c863b0b2a003fb8c5c8c4c0d9a10fdf78
cwrw <abeer.salameh@gmail.com>
2019-10-14 12:10:51 +0100

    wip for debugging

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,16 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += %i[
-  password
-  first_name
-  last_name
-  gender
-  birth_day
-  birth_month
-  birth_year
-  postcode
-  email
-  authenticity_token
-  token
-]
