55c7cd17a4ab7ef26d6bd4c4e76a9c70635fd0c7
Octavian Neguletu <octavian.neguletu@digital.education.gov.uk>
2019-10-22 15:53:40 +0100

    Add roles extraction with temp auth0 token

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,7 @@
+require 'uri'
+require 'net/http'
+require 'openssl'
+
 class PagesController < ApplicationController
   include Secured
 
@@ -7,6 +11,7 @@ class PagesController < ApplicationController
 
   def task_list
     @auth0_user_info = session[:userinfo]
+    @roles = user_roles(session[:userinfo]['uid'], session[:userinfo]['credentials']['token'])
 
     if user_session.job_profile_skills?
       current_job = JobProfile.find(user_session.job_profile_ids.first)
@@ -15,5 +20,17 @@ class PagesController < ApplicationController
     else
       @skills_summary_path = check_your_skills_path
     end
+  end
+
+  def user_roles(user_id, token)
+    url = URI("https://hidden-hill-8351.eu.auth0.com/api/v2/users/#{URI.encode(user_id)}/roles")
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Get.new(url)
+    token = ENV['AUTH0_TEMP_TOKEN']
+    request["authorization"] = "Bearer #{token}"
+    response = http.request(request)
+    response.read_body
   end
 end
