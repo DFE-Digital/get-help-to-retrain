2f088a4ff8b7f6e66abbfe22e8c001397dc2fc37
cwrw <abeer.salameh@gmail.com>
2019-10-23 07:53:56 +0100

    GET-490 Avoid session creation on homepage
    jira-ticket: https://dfedigital.atlassian.net/browse/GET-490
    
    Sessions are lazy loaded. If we don't touch teh session on
    the homepage we can avoid creating it unnecessarily.
    The three places the session is created and avoided are:
    1) csrf token: skip this tag if its the homepage
    2) start link: render the information page if the cookie
      is missing. The cookie is created when we have a session.
      Default to normal behaviour if the cookie exists
    3) authentication partial: default to return to saved results
      unless there is a cookie, where then we default to previous
      behaviour

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,15 @@
 class HomeController < ApplicationController
   def index
-    @pid_or_task_list_path = user_session.postcode.present? ? task_list_path : your_information_path
+    @pid_or_task_list_path = pid_or_task_list_path
+  end
+
+  private
+
+  def pid_or_task_list_path
+    if cookies['_get_help_to_retrain_session'].blank? || user_session.postcode.blank?
+      your_information_path
+    else
+      task_list_path
+    end
   end
 end
