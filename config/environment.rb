674ae39f97a557756d8dbf74fb61f2bc342dfda1
Octavian Neguletu <octavian.neguletu@digital.education.gov.uk>
2019-11-11 12:07:28 +0000

    Fix rails start error
    After intorducing both gems, we get the following
    error when rebooting the server:
    
    undefined method `formatter='
    for #<Logging::Logger:0x00007f82521ae650> (NoMethodError)
    
    This patching is suggested by:
    
    https://github.com/TwP/logging-rails/issues/27

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,21 @@
 # Load the Rails application.
 require_relative 'application'
 
+module Logging
+  module RailsCompat
+    def formatter
+      self
+    end
+
+    def tagged(*_args)
+      yield self
+    end
+
+    def current_tags
+      []
+    end
+  end
+end
+
 # Initialize the Rails application.
 Rails.application.initialize!
