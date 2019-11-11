685c2953dcabbd44704a58fa409c74f1e3173a12
Octavian Neguletu <octavian.neguletu@digital.education.gov.uk>
2019-11-11 16:39:38 +0000

    Reorder feature steps

--- spec/features/user_registration_spec.rb
+++ spec/features/user_registration_spec.rb
@@ -120,7 +120,19 @@ RSpec.feature 'User registration' do
     expect(page).to have_current_path(next_steps_path)
   end
 
-  scenario 'User can resume the journey from the point one saves results but gets the error page (NotifyService is down)' do
+  scenario 'When NotifyService is down, the user sees the correct error page when one tries to save results' do
+    allow(Notifications::Client).to receive(:new).and_raise(ArgumentError)
+
+    visit(job_profile_skills_path(job_profile_id: job_profile.slug))
+    click_on('Select these skills')
+    click_on('Save my results')
+    fill_in('email', with: 'test@test.test')
+    click_on('Save your results')
+
+    expect(page).to have_current_path(save_results_error_path)
+  end
+
+  scenario 'When NotifyService is down, user can resume the journey from the point one tries saving the results' do
     allow(Notifications::Client).to receive(:new).and_raise(ArgumentError)
 
     visit(job_profile_skills_path(job_profile_id: job_profile.slug))
@@ -133,18 +145,6 @@ RSpec.feature 'User registration' do
     click_on('Continue')
 
     expect(page).to have_current_path(training_hub_path)
-  end
-
-  scenario 'User sees the error page when one tries to save results NotifyService is down' do
-    allow(Notifications::Client).to receive(:new).and_raise(ArgumentError)
-
-    visit(job_profile_skills_path(job_profile_id: job_profile.slug))
-    click_on('Select these skills')
-    click_on('Save my results')
-    fill_in('email', with: 'test@test.test')
-    click_on('Save your results')
-
-    expect(page).to have_current_path(save_results_error_path)
   end
 
   scenario 'User can resume their journey to where they first clicked save my results' do
