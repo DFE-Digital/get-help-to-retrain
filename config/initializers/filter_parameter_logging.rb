1f493e6f0206139b5bf2bcf17ac53cbfbc6f3659
Octavian Neguletu <octavian.neguletu@digital.education.gov.uk>
2019-11-19 14:50:41 +0000

    Test

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -2,15 +2,6 @@
 
 # Configure sensitive parameters which will be filtered from the log file.
 Rails.application.config.filter_parameters += %i[
-  password
   first_name
   last_name
-  gender
-  birth_day
-  birth_month
-  birth_year
-  postcode
-  email
-  authenticity_token
-  token
 ]
