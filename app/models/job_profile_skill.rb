08af54ece56446aed65239f84eac8621d7d139bc
Andrew Retmanski <andrew.retmanski@digital.education.gov.uk>
2019-12-20 12:55:54 +0000

    Add rake task to persist rarity score for every skill

--- app/models/job_profile_skill.rb
+++ app/models/job_profile_skill.rb
@@ -1,4 +1,14 @@
 class JobProfileSkill < PrimaryActiveRecordBase
   belongs_to :job_profile
   belongs_to :skill
+
+  def self.update_rarity
+    rarity_ranking = select(:skill_id,
+                            'count(1) as times_occurring',
+                            'rank() over (order by count(1)) as rarity_rank').group(:skill_id)
+
+    rarity_ranking.all.each do |row|
+      Skill.update(row.skill_id, rarity: row.rarity_rank)
+    end
+  end
 end
