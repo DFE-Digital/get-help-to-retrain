37529b1a9f45f21c6665d7f2ad54b5a716594afa
cwrw <abeer.salameh@gmail.com>
2020-02-24 07:24:03 +0000

    GET-1020 Delete old records in csv importer
    jira-ticket: https://dfedigital.atlassian.net/browse/GET-1020
    
    Before importing, make sure to delete all old existing records
    for all csv tables. Also don't allow this task to be run in
    production.

--- app/services/csv/course_import_service.rb
+++ app/services/csv/course_import_service.rb
@@ -1,5 +1,30 @@
 module Csv
   class CourseImportService
-    def import(folder); end
+    def initialize
+      not_production!
+    end
+
+    def import(folder)
+      delete_old_records!
+    end
+
+    def import_stats
+      {}
+    end
+
+    private
+
+    def not_production!
+      raise 'Not to be run in production' if Rails.env.production?
+    end
+
+    def delete_old_records!
+      Csv::Provider.delete_all
+      Csv::Course.delete_all
+      Csv::Opportunity.delete_all
+      Csv::OpportunityStartDate.delete_all
+      Csv::Venue.delete_all
+      Csv::CourseLookup.delete_all
+    end
   end
 end
---
+++ spec/services/csv/course_import_service_spec.rb
@@ -0,0 +1,60 @@
+require 'rails_helper'
+
+RSpec.describe Csv::CourseImportService do
+  subject(:importer) { described_class.new }
+
+  let(:folder) { Rails.root.join('spec', 'fixtures', 'csv').to_s }
+
+  describe '#import' do
+    it 'deletes existing course records' do
+      create(:csv_course, name: 'Old course')
+      importer.import(folder)
+
+      expect(Csv::Course.where(name: 'Old course')).to be_empty
+    end
+
+    it 'deletes existing provider records' do
+      create(:provider, name: 'Old provider')
+      importer.import(folder)
+
+      expect(Csv::Provider.where(name: 'Old provider')).to be_empty
+    end
+
+    it 'deletes existing venue records' do
+      create(:venue, name: 'Old venue')
+      importer.import(folder)
+
+      expect(Csv::Venue.where(name: 'Old venue')).to be_empty
+    end
+
+    it 'deletes existing opportunity records' do
+      create(:opportunity, url: 'www.old-url.com')
+      importer.import(folder)
+
+      expect(Csv::Opportunity.where(url: 'www.old-url.com')).to be_empty
+    end
+
+    it 'deletes existing opportunity start date records' do
+      date = Date.parse('6-6-666')
+      create(:opportunity_start_date, start_date: date)
+      importer.import(folder)
+
+      expect(Csv::OpportunityStartDate.where(start_date: date)).to be_empty
+    end
+
+    it 'deletes existing course lookup records' do
+      create(:course_lookup, subject: 'old subject')
+      importer.import(folder)
+
+      expect(Csv::CourseLookup.where(subject: 'old subject')).to be_empty
+    end
+  end
+
+  describe '#import_stats' do
+    before { importer.import(folder) }
+
+    it 'reports statistics on completion' do
+      expect(importer.import_stats).to eq({})
+    end
+  end
+end
