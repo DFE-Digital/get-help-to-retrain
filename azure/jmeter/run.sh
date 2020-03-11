a045c42bacbb1ad28723961658706c47ef8e4363
Blagovest Karakashev <thebigbo1987@gmail.com>
2020-03-11 16:45:00 +0000

    wip

--- azure/jmeter/run.sh
+++ azure/jmeter/run.sh
@@ -12,9 +12,12 @@ echo "Test file: $testFile"
 echo "Host: $host"
 
 
-docker run --rm --name $NAME -i -v $rootPath:/test -w /test $IMAGE -Dlog_level.jmeter=DEBUG \
+docker run --rm --name $NAME -i -v $rootPath:/test -w /test $IMAGE -- -Dlog_level.jmeter=DEBUG \
   -Jhost=dev1.nrs-ghtr.org.uk \
   -n -t /test/jmeter-performance-tests.jmx -l ./test-plan.jtl -j ./jmeter.log -e -o ./report
 
 echo "==== HTML Test Report ===="
 echo "See HTML test report in ./report/index.html"
+
+
+-- -Dlog_level.jmeter=DEBUG -Jhost=dev1.nrs-ghtr.org.uk -n -t /test/jmeter-performance-tests.jmx -l ./test-plan.jtl -j ./jmeter.log -e -o ./report
