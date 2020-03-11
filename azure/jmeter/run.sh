05d394e89fe6fcb1cabaaefd6ccce64ae8c18eaa
Blagovest Karakashev <thebigbo1987@gmail.com>
2020-03-11 15:30:55 +0000

    JMeter-performance-tests

--- azure/jmeter/run.sh
+++ azure/jmeter/run.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
-#
-# Run JMeter Docker image with options
 
 NAME="jmetertest"
 IMAGE="justb4/jmeter:latest"
-ROOTPATH=$1
 
-echo "$ROOTPATH"
-# Finally run
-docker stop $NAME > /dev/null 2>&1
-docker rm $NAME > /dev/null 2>&1
-docker run --name $NAME -i -v $ROOTPATH:/test -w /test $IMAGE ${@:2}
+rootPath=$1
+testFile=$2
+host=$3
+
+echo "Root path: $rootPath"
+echo "Test file: $testFile"
+echo "Host: $host"
+
+
+docker run --rm --name $NAME -i -v $rootPath:/test -w /test $IMAGE -Dlog_level.jmeter=DEBUG \
+  -Jhost=dev1.nrs-ghtr.org.uk \
+  -n -t /test/jmeter-performance-tests.jmx -l ./test-plan.jtl -j ./jmeter.log -e -o ./report
+
+echo "==== HTML Test Report ===="
+echo "See HTML test report in ./report/index.html"
