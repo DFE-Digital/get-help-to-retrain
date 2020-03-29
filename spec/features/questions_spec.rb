afcd325e35568d3f6fdd46f026519467c8d2ebc8
Octavian Neguletu <octavian.neguletu@digital.education.gov.uk>
2020-03-29 15:06:11 +0100

    Redesign the breadcrumbs for the overall service
    For more consistency across user journey we would like
    to update the breadcrumbs across the whole service as follows:
    
    Home > Your information
    Home > Steps
    Home > Steps > Check your skills
    Home > Steps > Check your skills > Check your skills results
    Home > Steps > Check your skills > Search results > Current job skills
    Home > Steps > Your skills
    Home > Steps > Check your skills
    Home > Steps > Your job matches
    Home > Steps > Your job matches > Job profile details
    Home > Steps > Action plan
    Home > Steps > Action plan > Courses near you
    Home > Steps > Action plan > Courses near you
    Home > Steps > Action plan > CV advice
    Home > Steps > Action plan > Cover letter advice
    Home > Steps > Action plan > Interview advice
    Home > Steps > Action plan > Offers near you
    Home > Steps > Action plan > Jobs near you

--- spec/features/questions_spec.rb
+++ spec/features/questions_spec.rb
@@ -9,11 +9,48 @@ RSpec.feature 'Questions' do
     expect(page).to have_current_path(training_questions_path)
   end
 
+  scenario 'User can go back to previous page from training questions page' do
+    user_targets_job
+
+    click_on('Back')
+
+    expect(page).to have_current_path(job_profile_path(job_profile.slug))
+  end
+
+  scenario 'User defaults to going back to root path when deeplinking to training questions page on a non empty session and then hitting Back' do
+    user_targets_job
+
+    visit(training_questions_path)
+
+    click_on('Back')
+
+    expect(page).to have_current_path(root_path)
+  end
+
   scenario 'User sees IT training questions when targetting a job' do
     user_targets_job
     click_on('Continue')
 
     expect(page).to have_current_path(it_training_questions_path)
+  end
+
+  scenario 'User can go back to previous page from IT training questions page' do
+    user_targets_job
+    click_on('Continue')
+
+    click_on('Back')
+
+    expect(page).to have_current_path(training_questions_path)
+  end
+
+  scenario 'User defaults to going back to root path when deeplinking to it training questions page on a non empty session and then hitting Back' do
+    user_targets_job
+
+    visit(it_training_questions_path)
+
+    click_on('Back')
+
+    expect(page).to have_current_path(root_path)
   end
 
   scenario 'User sees job hunting questions when targetting a job' do
@@ -22,6 +59,26 @@ RSpec.feature 'Questions' do
     click_on('Continue')
 
     expect(page).to have_current_path(job_hunting_questions_path)
+  end
+
+  scenario 'User can go back to previous page from job hunting questions page' do
+    user_targets_job
+    click_on('Continue')
+    click_on('Continue')
+
+    click_on('Back')
+
+    expect(page).to have_current_path(it_training_questions_path)
+  end
+
+  scenario 'User defaults to going back to root path when deeplinking to job hunting questions page on a non empty session and then hitting Back' do
+    user_targets_job
+
+    visit(job_hunting_questions_path)
+
+    click_on('Back')
+
+    expect(page).to have_current_path(root_path)
   end
 
   scenario 'User navigates to action plan after going through questions' do
