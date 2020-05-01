27e0172486e319bd751332b77b620357557ec272
Octavian Neguletu <octavian.neguletu@digital.education.gov.uk>
2020-05-01 12:45:47 +0100

    Refactor date of birth validation on PID form
    Adding more granular date of birth validation for
    the PID form for the following cases:
    
    - If no date is entered
    - If an incomplete date is entered - missing day
    - If an incomplete date is entered - missing month
    - If an incomplete date is entered - missing year
    - If an incomplete date is entered - missing day and month
    - If an incomplete date is entered - missing day and year
    - If an incomplete date is entered - missing month and year
    - If the date entered is not a real one/not number characters
    - If the date is in the future when it needs to be in the past
    - If the date of birth is earlier than 1900

--- spec/factories/user_personal_data.rb
+++ spec/factories/user_personal_data.rb
@@ -5,5 +5,8 @@ FactoryBot.define do
     postcode { ['NW11 8QE', 'NW11 7PT', 'NW11 7HB', 'NW10 7SF', 'NW10 7NZ', 'NW10 6HJ'].sample }
     gender { %w[male female not_mentioned].sample }
     dob { Faker::Date.between(from: 99.years.ago, to: 18.years.ago) }
+    birth_day { 13 }
+    birth_month { 1 }
+    birth_year { DateTime.now.year - 18 }
   end
 end
