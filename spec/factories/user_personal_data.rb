ba398426b1251620b0390be72d7126f6875f14b4
Octavian Neguletu <octavian.neguletu@digital.education.gov.uk>
2020-05-01 13:15:36 +0100

    Update date of birth validation for ActiveAdmin
    Active Admin is using a different convention for
    the date of birth which is:
    - dob(1i) - year,
    - dob(2i) - month,
    - dob(3i) - day,
    which is different from our service that is using:
    - user_personal_data[birth_day],
    - user_personal_data[birth_month],
    - user_personal_data[birth_year].
    
    So date_format validation has to only kick in if
    date of birth is not set, otherwise admin users will
    not be able to update it from the ActiveAdmin
    if they wants to.

--- spec/factories/user_personal_data.rb
+++ spec/factories/user_personal_data.rb
@@ -4,7 +4,6 @@ FactoryBot.define do
     last_name { Faker::Name.last_name }
     postcode { ['NW11 8QE', 'NW11 7PT', 'NW11 7HB', 'NW10 7SF', 'NW10 7NZ', 'NW10 6HJ'].sample }
     gender { %w[male female not_mentioned].sample }
-    dob { Faker::Date.between(from: 99.years.ago, to: 18.years.ago) }
     birth_day { 13 }
     birth_month { 1 }
     birth_year { DateTime.now.year - 18 }
