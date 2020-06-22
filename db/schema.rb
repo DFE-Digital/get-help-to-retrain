951aa1e77bffd068827a5116f0f4cdb632b99367
Octavian Neguletu <octavian.neguletu@digital.education.gov.uk>
2020-06-22 15:33:31 +0100

    Add a master name field to skills for mapping

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_03_100607) do
+ActiveRecord::Schema.define(version: 2020_06_22_142925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 2020_04_03_100607) do
     t.datetime "updated_at", null: false
     t.boolean "enabled", default: true, null: false
     t.integer "rarity"
+    t.string "master_name"
     t.index ["enabled"], name: "index_skills_on_enabled"
     t.index ["name"], name: "index_skills_on_name"
     t.index ["rarity"], name: "index_skills_on_rarity"
